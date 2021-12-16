fa39c9b5a0533a8d8c4ca1f2471da5aab0101d8f
Gavin Ray <ray.gavin97@gmail.com>
2021-12-16 18:40:47 -0500

    Make every Athena setting configurable

--- src/aws-athena-sql.ts
+++ src/aws-athena-sql.ts
@@ -1,4 +1,4 @@
-import { AthenaExpress } from "athena-express"
+import { AthenaExpress, ConnectionConfigInterface } from "athena-express"
 import AWS from "aws-sdk"
 import {
   AWS_ACCESS_KEY_ID,
@@ -101,15 +101,84 @@ export async function exportAthenaTablesToHasuraMetadata(params: AWS.Athena.List
   return { hasuraMetadata, schemaEndpointMetadata }
 }
 
-export const athena = new AthenaExpress({
+// REF: https://github.com/ghdna/athena-express#advance-config-parameters
+const athenaExpressConfig: Partial<ConnectionConfigInterface> = {
   aws: AWS, // required
+  /**
+   * The location in Amazon S3 where your query results are stored, such as s3://path/to/query/bucket/.
+   * athena-express will create a new bucket for you if you don't provide a value for this param but
+   * sometimes that could cause an issue if you had recently deleted a bucket with the same name.
+   * (something to do with cache).
+   * 
+   * When that happens, just specify you own bucket name.
+   * Alternatively you can also use workgroup.
+   */
   s3: AWS_S3_RESULT_BUCKET_ADDRESS, // optional
+  /**
+   * Athena database name that the SQL queries should be executed in.
+   * When a db name is specified in the config, you can execute SQL queries without needing to
+   * explicitly mention DB name.
+   * 
+   * e.g.
+   * athenaExpress.query("SELECT * FROM movies LIMIT 3")
+   * 
+   * as opposed to
+   * athenaExpress.query({sql: "SELECT * FROM movies LIMIT 3", db: "moviedb"});
+   */
   db: AWS_ATHENA_DB_NAME, // optional
+  /**
+   * The catalog to which the query results belong
+   */
   catalog: AWS_ATHENA_CATALOG_NAME, // optional
-  formatJson: true, // optional
-  getStats: true, // optional
-  ignoreEmpty: false,
-})
+  /**
+   * Override as false if you rather get the raw unformatted output from S3.
+   */
+  formatJson: process.env["AWS_ATHENA_OPTION_FORMAT_JSON"] ? // optional
+    Boolean(process.env["AWS_ATHENA_OPTION_FORMAT_JSON"])
+    : true,
+  /**
+   * Set getStats: true to capture additional metadata for your query
+   */
+  getStats: process.env["AWS_ATHENA_OPTION_GET_QUERY_STATS"]  // optional
+    ? Boolean(process.env["AWS_ATHENA_OPTION_GET_QUERY_STATS"])
+    : true,
+  /**
+   * Ignore fields with empty values from the final JSON response.
+
+   */
+  ignoreEmpty: process.env["AWS_ATHENA_OPTION_IGNORE_EMPTY_FIELDS"] ? // optional
+    Boolean(process.env["AWS_ATHENA_OPTION_IGNORE_EMPTY_FIELDS"])
+    : true,
+  /**
+   * The name of the workgroup in which the query is being started.
+   * Note: athena-express cannot create workgroups (as it includes a lot of configuration)
+   * so you will need to create one beforehand IFF you intend to use a non default workgroup
+   */
+  workgroup: process.env["AWS_ATHENA_OPTION_WORKGROUP"]  // optional
+    ? process.env["AWS_ATHENA_OPTION_WORKGROUP"]
+    : "primary",
+  /**
+   * Wait interval between re-checking if the specific Athena query has finished executing
+   */
+  retry: process.env["AWS_ATHENA_OPTION_RETRY_MS"]  // optional
+    ? Number(process.env["AWS_ATHENA_OPTION_RETRY_MS"])
+    : 200,
+  ...(
+    process.env["AWS_ATHENA_OPTION_ENCRYPTION_ENABLED"] && { // optional
+      /**
+       * Encryption configuation example usage:
+       * { EncryptionOption: "SSE_KMS", KmsKey: process.env.kmskey }
+       */
+      encryption: {
+        EncryptionOption: process.env["AWS_ATHENA_OPTION_ENCRYPTION_TYPE"] ?? "SSE_KMS",
+        [process.env["AWS_ATHENA_OPTION_ENCRYPTION_KEY"] ?? "KmsKey"]: process.env["AWS_ATHENA_OPTION_ENCRYPTION_VALUE"] ?? "",
+      }
+    }
+  )
+}
+
+console.log("CONFIGURING ATHENA CLIENT WITH PARAMETERS: ", athenaExpressConfig)
+export const athena = new AthenaExpress(athenaExpressConfig)
 
 // Example for quick testing -- call this if you need to test connection/config and query results
 async function testAthena() {
